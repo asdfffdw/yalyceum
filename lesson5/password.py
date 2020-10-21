083afe435e3ee0dae4863b912c59f7ca5e1eec72
windev <sktekk@yahoo.com>
2020-10-21 23:07:26 +0300

    Новые упражнения

---
+++ lesson5/password.py
@@ -0,0 +1,17 @@
+password1 = input()
+password2 = input()
+x = False
+while x is False:
+    if len(password1) < 8:
+        print('Короткий')
+    elif '123' in password1:
+        print('Простой!')
+    elif password1 != password2:
+        print('Различаются')
+    else:
+        print('OK')
+        x = True
+        quit()
+    password1 = input()
+    password2 = input()
+
