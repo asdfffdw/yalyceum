0e6f753af7c9993e3499c9d3f80bc40422a4d5c3
windev <sktekk@yahoo.com>
2020-10-27 22:05:06 +0300

    new ide

--- lesson8/FindCatBreak.py
+++ lesson8/FindCatBreak.py
@@ -1,11 +1,11 @@
 x = int(input())
 i = 1
 while i <= x:
-	i += 1
-	y = input()
-	if ('Кот' in y) or ('кот' in y):
-		print('МЯУ')
-		break
+    i += 1
+    y = input()
+    if ('Кот' in y) or ('кот' in y):
+        print('МЯУ')
+        break
 if not ('кот' in y) or ('Кот' in y):
-	print("НЕТ")
-print()
+    print("НЕТ")
+
